e73218afa0cacb40358884973a9d1bed09e2c9ad
Stephen Ohimor <sohimor@dokkit.com>
2012-08-08 08:38:46 -0400

    added anonymous class examples

--- src/main/java/com/mangrove/core/Anonymous.java
+++ src/main/java/com/mangrove/core/Anonymous.java
@@ -3,5 +3,57 @@ package com.mangrove.core;
 public class Anonymous {
 
 	
+	private InnerClass innerClass;
+
+	public InnerClass getInnerClass() {
+		return innerClass;
+	}
+
+	public void setInnerClass(InnerClass innerClass) {
+		this.innerClass = innerClass;
+	}
+
+	private class InnerClass	{
+		private String name = "private inner class";
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+	}
 	
+}
+
+class DefaultInnerClass{
+	private String name= "default inner class";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+protected class ProtectedInnerClass{
+	private String name = "protected inner class";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
+
+
+}//end main class
+
+//if its a top level class it must be public, default,, abstract or final
+abstract class ProtectedAbstractTopLevelClass{
+	
+	public abstract String thatsNotMyName();
 }
