ad111b08ac9f27779223679da0db20690f438a57
Stephen Ohimor <saodesign@gmail.com>
2012-09-25 23:40:53 -0400

    fewfew
    fewfew

---
+++ src/main/java/com/mangrove/examples/Vehicle.java
@@ -0,0 +1,19 @@
+package com.mangrove.examples;
+
+public abstract class Vehicle {
+
+	abstract public void speed();
+}
+
+abstract class Car extends Vehicle{
+	public static void main (String args[]) {
+	Vehicle ob1;        
+	//ob1=new Car();   //1
+
+	}
+public Car(){
+	
+}
+
+}
+
